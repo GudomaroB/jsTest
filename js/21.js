bd19de2ef51a9b2a054f913584780728669f6a42
gudo <new_dayff@internet.ru>
2023-03-12 23:51:49 +0500

    update 21 files js

--- js/21.js
+++ js/21.js
@@ -5,6 +5,29 @@
 // ? ЦВЕТ ТЕКСТА
 // * ЦВЕТ ТЕКСТА
 
-// ! ТЕМА УРОКА
+// ! ТЕМА УРОКА ООП
 
 //! пример 1
+const solder = {
+  healt: 500,
+  armor: 100,
+  seyHello: function () {
+    console.log("Привет");
+  },
+};
+const Vassilyi = {
+  healt: 100,
+};
+Object.setPrototypeOf(Vassilyi, solder);
+Vassilyi.seyHello();
+
+//! пример 1
+const solders = {
+  healt: 500,
+  armor: 100,
+  seyHelloO: function () {
+    console.log("Привет");
+  },
+};
+const vassilyi = Object.create(solders);
+vassilyi.seyHelloO();
